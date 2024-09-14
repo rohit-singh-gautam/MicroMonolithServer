0121c9bd54e33ecc8ce81bf5208be2a9aefecf2e
Rohit Jairaj Singh <rohit@singh.org.in>
2024-09-14 10:33:34 +0530

    removed duplicate declaration of values.

--- core/test/quictest.cpp
+++ core/test/quictest.cpp
@@ -12,6 +12,10 @@
 #include <mms/net/quic.h>
 #include <gtest/gtest.h>
 
+std::vector<uint64_t> values16 { 16383, 64, 65, 88, 330, 2345, 2034, 3934, 12445 };
+std::vector<uint64_t> values32 { 16384, 1073741823ULL, 20000, 8000000ULL, 330345677ULL, 43455555ULL, 203432423ULL, 39342344ULL, 124544343ULL };
+std::vector<uint64_t> values64 { 4611686018427387903ULL, 1073741824ULL, 4611686018427387ULL, 80000000000ULL, 330345643577ULL, 4345553334455ULL, 2034324255553ULL, 39342355553344ULL, 1245433334343ULL };
+
 auto EncodeAndDecode(uint64_t value, MMS::FullStream &stream) {
     stream.Reset();
     MMS::net::quic::EncodeUnsignedInteger(value, stream);
@@ -31,9 +35,8 @@ TEST(QUICTest, IntegerTestByte) {
 }
 
 TEST(QUICTest, IntegerTestShort) {
-    std::vector<uint64_t> values { 16383, 64,  65, 88, 330, 2345, 2034, 3934, 12445 };
     MMS::FullStreamAutoAlloc stream { 17 };
-    for(auto value: values) {
+    for(auto value: values16) {
         auto [decoded, size] = EncodeAndDecode(value, stream);
         EXPECT_EQ(size, 2);
         EXPECT_EQ(value, decoded);
@@ -41,9 +44,8 @@ TEST(QUICTest, IntegerTestShort) {
 }
 
 TEST(QUICTest, IntegerTestU32) {
-    std::vector<uint64_t> values { 16384, 1073741823ULL, 20000, 8000000ULL, 330345677ULL, 43455555ULL, 203432423ULL, 39342344ULL, 124544343ULL };
     MMS::FullStreamAutoAlloc stream { 33 };
-    for(auto value: values) {
+    for(auto value: values32) {
         auto [decoded, size] = EncodeAndDecode(value, stream);
         EXPECT_EQ(size, 4);
         EXPECT_EQ(value, decoded);
@@ -51,9 +53,8 @@ TEST(QUICTest, IntegerTestU32) {
 }
 
 TEST(QUICTest, IntegerTestU64) {
-    std::vector<uint64_t> values { 4611686018427387903ULL, 1073741824ULL, 4611686018427387ULL, 80000000000ULL, 330345643577ULL, 4345553334455ULL, 2034324255553ULL, 39342355553344ULL, 1245433334343ULL };
     MMS::FullStreamAutoAlloc stream { 65 };
-    for(auto value: values) {
+    for(auto value: values64) {
         auto [decoded, size] = EncodeAndDecode(value, stream);
         EXPECT_EQ(size, 8);
         EXPECT_EQ(value, decoded);
