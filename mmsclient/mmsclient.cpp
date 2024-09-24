11b15cdbcb92c6a9bba721573aaf87388a300b20
Rohit Jairaj Singh <rohit@singh.org.in>
2024-09-24 09:36:00 +0530

    Added MMS Client skeleton.

---
+++ mmsclient/mmsclient.cpp
@@ -0,0 +1,17 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// Author: Rohit Jairaj Singh (rohit@singh.org.in)                                         //
+// Copyright (c) 2024 Rohit Jairaj Singh. All rights reserved.                             //
+// This code is proprietary and confidential. Unauthorized copying of this file, via any   //
+// medium, is strictly prohibited.                                                         //
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <vector>
+#include <string>
+#include <iostream>
+
+
+int main(int argc, char *argv[]) {
+    const std::vector<std::string> args {argv, argv + argc};
+    
+    return 0;
+}
