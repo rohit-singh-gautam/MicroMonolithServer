e111341182969099203b5f6368d7cc7b98fcafcf
Rohit Jairaj Singh <rohit@singh.org.in>
2024-10-25 17:33:12 +0530

    Changed license to GPL 3.0

--- library/server/src/http.cpp
+++ library/server/src/http.cpp
@@ -1,9 +1,19 @@
-/////////////////////////////////////////////////////////////////////////////////////////////
-// Author: Rohit Jairaj Singh (rohit@singh.org.in)                                         //
-// Copyright (c) 2024 Rohit Jairaj Singh. All rights reserved.                             //
-// This code is proprietary and confidential. Unauthorized copying of this file, via any   //
-// medium, is strictly prohibited.                                                         //
-/////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2024  Rohit Jairaj Singh (rohit@singh.org.in)          //
+//                                                                      //
+// This program is free software: you can redistribute it and/or modify //
+// it under the terms of the GNU General Public License as published by //
+// the Free Software Foundation, either version 3 of the License, or    //
+// (at your option) any later version.                                  //
+//                                                                      //
+// This program is distributed in the hope that it will be useful,      //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of       //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the        //
+// GNU General Public License for more details.                         //
+//                                                                      //
+// You should have received a copy of the GNU General Public License    //
+// along with this program.  If not, see <https://www.gnu.org/licenses/>//
+//////////////////////////////////////////////////////////////////////////
 
 #include <mms/server/http.h>
 #include <format>
